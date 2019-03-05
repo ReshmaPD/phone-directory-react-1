df29c95573bd863d579efd011b94524a99f13ab2
Reshma D <43452215+ReshmaPD@users.noreply.github.com>
2019-03-06 01:23:11 +0530

    Update SubscriberInputForm.js

--- src/components/SubscriberInputForm.js
+++ src/components/SubscriberInputForm.js
@@ -14,7 +14,7 @@ class SubscriberInputForm extends Component {
   };
   render() {
     return (
-      <div>
+      <React.Fragment>
         <form onSubmit={this.onSubmit} className="form">
           <label>Name</label>
           <input
@@ -23,18 +23,21 @@ class SubscriberInputForm extends Component {
             ref={_nameInputElement =>
               (this._nameInputElement = _nameInputElement)
             }
+            required
           />
           <label>Phone</label>
           <input
             type="text"
             name="contact number"
+            pattern="[1-9]{1}[0-9]{9}"
             ref={_numberInputElement =>
               (this._numberInputElement = _numberInputElement)
             }
+             required
           />
           <button className="button add-button">Add Subscriber</button>
         </form>
-      </div>
+      <React.Fragment>
     );
   }
 }
